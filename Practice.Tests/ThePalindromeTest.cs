97a2d84a52c3a56cfb7889a769ca6c1b07c0aea2
kirill <kirill.epsilonlambda@gmail.com>
2016-03-16 01:07:00 -0400

    [ThePalindrome] First solution attempt, O(n)

---
+++ Practice.Tests/ThePalindromeTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EpsilonLambda.Competitive.TopCoder.Practice.Tests
+{
+    [TestClass]
+    public class ThePalindromeTest
+    {
+        ThePalindrome thePalindrome;
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            thePalindrome = new ThePalindrome();
+        }
+
+        [TestMethod]
+        public void MaxPalindromeSuffix_Regular()
+        {
+            Assert.AreEqual("bab", thePalindrome.FindMaxPalindromeSuffix("abab"));
+        }
+
+        [TestMethod]
+        public void MaxPalindromeSuffix_Edge()
+        { 
+            Assert.AreEqual("y", thePalindrome.FindMaxPalindromeSuffix("qwerty"));
+            Assert.AreEqual("", thePalindrome.FindMaxPalindromeSuffix(""));
+            Assert.AreEqual("abacaba", thePalindrome.FindMaxPalindromeSuffix("abacaba"));
+        }
+
+        [TestMethod]
+        public void Ex0()
+        {
+            Assert.AreEqual(5, thePalindrome.find("abab"));
+        }
+
+        [TestMethod]
+        public void Ex1()
+        {
+            Assert.AreEqual(7, thePalindrome.find("abacaba"));
+        }
+
+        [TestMethod]
+        public void Ex2()
+        {
+            Assert.AreEqual(11, thePalindrome.find("qwerty"));
+        }
+
+        [TestMethod]
+        public void Ex3()
+        {
+            Assert.AreEqual(38, thePalindrome.find("abdfhdyrbdbsdfghjkllkjhgfds"));
+
+        }
+    }
+}
---
+++ Practice/ThePalindrome.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EpsilonLambda.Competitive.TopCoder.Practice
+{
+    public class ThePalindrome
+    {
+        public int find(string s)
+        {
+            var maxPalindromeSuffix = FindMaxPalindromeSuffix(s);
+            return s.Length + s.Length - maxPalindromeSuffix.Length;
+        }
+
+        internal string FindMaxPalindromeSuffix(string s)
+        {
+            if (s == string.Empty)
+                return string.Empty;
+
+            var starterPositions = new Queue<int>(FindIndicesOfChar(s.Last(), s));
+
+            int candidateStart = 0;
+            int candidateFwPtr = candidateStart;
+            int candidateBkPtr = s.Length - 1;
+
+            while (candidateFwPtr < candidateBkPtr)
+            {
+                if (s[candidateBkPtr] == s[candidateFwPtr])
+                {
+                    candidateFwPtr++;
+                    candidateBkPtr--;
+                }
+                else
+                {
+                    ReinitializePointers(ref candidateStart, ref candidateFwPtr, ref candidateBkPtr, starterPositions, s);
+                }
+            }
+
+            return s.Substring(candidateStart);
+        }
+
+        internal void ReinitializePointers(ref int candidateStart,
+            ref int candidateFwPtr,
+            ref int candidateBkPtr,
+            Queue<int> starterPositions,
+            string s)
+        {
+            if (starterPositions.Count == 0)
+                throw new Exception();
+
+            candidateStart = starterPositions.Dequeue();
+            candidateFwPtr = candidateStart;
+            candidateBkPtr = s.Length - 1;
+        }
+
+        internal int[] FindIndicesOfChar(char needle, string haystack)
+        {
+            return haystack.Select((character, index) => new KeyValuePair<char, int>(character, index))
+                                                    .Where(kvp => kvp.Key == needle)
+                                                    .Select(kvp => kvp.Value).ToArray();
+
+        }
+
+    }
+}
