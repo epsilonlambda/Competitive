9581a29c7977b38d84046171071bdf7651dcc8de
kirill <kirill.epsilonlambda@gmail.com>
2016-03-26 15:15:17 -0400

    [Timed Practice] DivFreed2

---
+++ Practice.Tests/DivFreed2Tests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EpsilonLambda.Competitive.TopCoder.Practice.Tests
+{
+    /// <summary>
+    /// Summary description for DivFreed2Tests
+    /// </summary>
+    [TestClass]
+    public class DivFreed2Tests
+    {
+        public DivFreed2Tests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+
+        DivFreed2 divFreed2;
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            divFreed2 = new DivFreed2();
+        }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Ex0()
+        {
+            Assert.AreEqual(3, divFreed2.count(2, 2));
+        }
+
+        [TestMethod]
+        public void Ex1()
+        {
+            Assert.AreEqual(1, divFreed2.count(9, 1));
+        }
+
+        [TestMethod]
+        public void Ex2()
+        {
+            Assert.AreEqual(15, divFreed2.count(3, 3));
+        }
+
+        [TestMethod]
+        public void Ex3()
+        {
+            Assert.AreEqual(107, divFreed2.count(1, 107));
+        }
+
+        [TestMethod]
+        public void Ex4()
+        {
+            Assert.AreEqual(83, divFreed2.count(2, 10));
+        }
+
+        [TestMethod]
+        public void Ex5()
+        {
+            Assert.AreEqual(1515011, divFreed2.count(2, 1234));
+        }
+
+        [TestMethod]
+        public void Ex6()
+        {
+            Assert.AreEqual(326, divFreed2.count(3, 8));
+        }
+
+        [TestMethod]
+        public void Ex7()
+        {
+            Assert.AreEqual(526882214, divFreed2.count(10, 100000));
+        }
+    }
+}
---
+++ Practice/DivFreed2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpsilonLambda.Competitive.TopCoder.Practice
+{
+    public class DivFreed2
+    {
+        public int count(int n, int k)
+        {
+            ubound = k;
+            lbound = 1;
+
+            var firstElements = getRange(lbound, ubound);
+
+            var solutions = getSolutions(n, k, firstElements);
+
+            // int overflow?
+            return solutions.Select(s => s.Count).Sum() % 1000000007;
+        }
+
+        public class SolutionInfo
+        {
+            public int First { get; private set; }
+            public int Count { get; private set; }
+
+            public SolutionInfo(int first, int count)
+            {
+                First = first;
+                Count = count;
+            }
+        }
+
+        int ubound, lbound;
+        public IEnumerable<SolutionInfo> getSolutions(int n, int k, IEnumerable<int> firstItemCandidates)
+        {
+            if (n == 1) return firstItemCandidates.Select(x => new SolutionInfo(x, 1)); // TODO: uzhas!
+
+            List<SolutionInfo> solutions = new List<SolutionInfo>();
+
+            foreach (int x in firstItemCandidates)
+            {
+                var leqRange = getRange(x, k);
+                var leqSolutions = getSolutions(n - 1, k, leqRange);//
+
+                var modRange = getRange(lbound, ubound).Where(m => x % m != 0);
+                var modRangeToScan = modRange.Except(leqSolutions.Select(s => s.First).Distinct());
+                var modSolutions = getSolutions(n - 1, k, modRangeToScan);
+
+                var allSolutions = leqSolutions.Concat(modSolutions).GroupBy(sol => sol.First, sol => sol.First).Select(g => new SolutionInfo(g.Key, g.Sum()));
+
+                solutions.Add(new SolutionInfo(x, allSolutions.Count()));
+            }
+
+            return solutions;
+
+        }
+
+
+        public IEnumerable<int> getRange(int lbound, int ubound)
+        {
+            var size = ubound - lbound + 1;
+            var range = new int[size];
+
+            for(int i = 0; i< size; i++)
+            {
+                range[i] = lbound + i;
+            }
+
+            return range;
+        }
+    }
+}
