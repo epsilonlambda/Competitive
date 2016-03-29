ab54a1dc535efbed73b964b8f1cffa268e9e5024
kirill <kirill.epsilonlambda@gmail.com>
2016-03-28 20:10:48 -0400

    [MoveStonesEasy] SRM Timed Practice original solution

---
+++ Practice.Tests/MoveStonesEasyTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EpsilonLambda.Competitive.TopCoder.Practice.Tests
+{
+    /// <summary>
+    /// Summary description for MoveStonesEasyTest
+    /// </summary>
+    [TestClass]
+    public class MoveStonesEasyTest
+    {
+        public MoveStonesEasyTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        MoveStonesEasy moveStonesEasy;
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            moveStonesEasy = new MoveStonesEasy();
+        }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Ex0()
+        {
+            Assert.AreEqual(1, moveStonesEasy.get(new int[] { 1, 2 }, new int[] { 2, 1 }));
+        }
+        [TestMethod]
+        public void Ex1()
+        {
+            Assert.AreEqual(10, moveStonesEasy.get(new int[] { 10, 0 }, new int[] { 0, 10 }));
+        }
+        [TestMethod]
+        public void Ex2()
+        {
+            Assert.AreEqual(2, moveStonesEasy.get(new int[] { 0, 0, 1 }, new int[] { 1, 0, 0 }));
+        }
+        [TestMethod]
+        public void Ex3()
+        {
+            Assert.AreEqual(0, moveStonesEasy.get(new int[] { 12, 12 }, new int[] { 12, 12 }));
+        }
+        [TestMethod]
+        public void Ex4()
+        {
+            Assert.AreEqual(-1, moveStonesEasy.get(new int[] { 5 }, new int[] { 6 }));
+        }
+        [TestMethod]
+        public void Ex5()
+        {
+            Assert.AreEqual(9, moveStonesEasy.get(new int[] { 3, 10, 0, 4, 0, 0, 0, 1, 0 }, new int[] { 5, 5, 0, 7, 0, 0, 0, 0, 1 }));
+        }
+    }
+}
---
+++ Practice/MoveStonesEasy.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpsilonLambda.Competitive.TopCoder.Practice
+{
+    public class MoveStonesEasy
+    {
+        public int get(int[] a, int[] b)
+        {
+            int nPiles = a.Length;
+            int moves = 0;
+            for(int currentFixCandidate = 0; currentFixCandidate < nPiles; currentFixCandidate++)
+            {
+                var extras = a[currentFixCandidate] - b[currentFixCandidate];
+                if (extras >= 0) continue;
+
+                System.Diagnostics.Debug.Assert(extras < 0);
+                var required = Math.Abs(extras);
+
+                var searchDistance = 0;
+                while (required > 0)
+                {
+                    var nextExtraSourceIndex = GetExtraSourceIndex(currentFixCandidate, a, b, searchDistance);
+                    if (nextExtraSourceIndex < 0) return -1;
+                    searchDistance = Math.Abs(currentFixCandidate - nextExtraSourceIndex);
+
+                    var extraAvailable = a[nextExtraSourceIndex] - b[nextExtraSourceIndex];
+
+                    var transferCount = Math.Min(required, extraAvailable);
+                    var transferDistance = Math.Abs(currentFixCandidate - nextExtraSourceIndex);
+                    a[nextExtraSourceIndex] -= transferCount;
+                    a[currentFixCandidate] += transferCount;
+                    moves += (transferCount * transferDistance);
+                    required -= transferCount;
+                }
+            }
+
+            return moves;
+        }
+
+        internal int GetExtraSourceIndex(int currentFixCandidate, int[] a, int[] b, int searchStartDistance = 0)
+        {
+            int maxDistance = Math.Max(currentFixCandidate, a.Length - currentFixCandidate - 1);
+            for(int distance = 1; distance <= maxDistance; distance++)
+            {
+                int leftChoice = currentFixCandidate - distance;
+                int rightChoice = currentFixCandidate + distance; // right, not necessarily correct...
+
+                var choices = new int[] { leftChoice, rightChoice }.Where(candidate => HasExtras(candidate, a, b));
+                if (choices.Any())
+                    return choices.First();
+            }
+
+            return -1;
+        }
+
+        internal bool HasExtras(int i, int[] a, int[] b)
+        {
+            return (i >= 0 && i < a.Length)
+                && (a[i] - b[i] > 0);
+        }
+    }
+}
