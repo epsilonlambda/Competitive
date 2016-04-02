f5f2dfd52135d9b924d8e5837b64db5542c310d8
kirill <kirill.epsilonlambda@gmail.com>
2016-04-02 13:33:52 -0400

    [CombiningSlimes] Timed Practice
    * Proof of correctness/optimality is a bit trickier than the trivial algorithm that solves it.
    * Simple example of the cross-domain structural induction approach.

---
+++ Practice/CombiningSlimes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpsilonLambda.Competitive.TopCoder.Practice
+{
+    public class CombiningSlimes
+    {
+        public int maxMascots(int[] a)
+        {
+            var slimes = new List<int>(a);
+
+            int score = 0;
+            while(slimes.Count > 1)
+            {
+                var first = slimes.Pop();
+                var second = slimes.Pop();
+
+                slimes.Add(first + second);
+
+                score += (first * second);
+            }
+
+            return score;
+
+        }
+    }
+
+    public static class ListExtension
+    {
+        public static T Pop<T>(this List<T> lst)
+        {
+            var x = lst.First();
+            lst.RemoveAt(0);
+            return x;
+        }
+    }
+}
