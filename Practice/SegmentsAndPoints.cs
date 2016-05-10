7aa55758c8dd4e904d0cd47f24d185f47a134c4f
kirill <kirill.epsilonlambda@gmail.com>
2016-05-09 22:35:46 -0400

    [Practice] SegmentsAndPoints
    * Greedy solution that imposes a forced ordering on the segments. Can prove that forced ordering gives some solution IFF a solution exists.
    * Solution is quadratic [ O(nPoints x nSegments) ], but can be improved for some cases with additional checks and cleanup of the segments array (eg. removing all segments that finish before the current point up front in each iteration as we're scanning, etc.)

--- Practice/SegmentsAndPoints.cs
+++ Practice/SegmentsAndPoints.cs
@@ -1,63 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EpsilonLambda.Competitive.TopCoder.Practice
 {
     public class SegmentsAndPoints
     {
-        public string isPossible(int[] p, int[] l, int[] r)
+        String isPossible(int[] p, int[] l, int[] r)
         {
-            int n = p.Length;
+            Array.Sort(p);
+            var segments = l.Zip(r, (left, right) => new Tuple<int, int>(left, right)).OrderBy(seg => seg.Item2).ToList();
 
-            IEnumerable<Point> allPoints = p.Select((x, id) => new Point(Point.PType.POINT, x, id));
-            allPoints = allPoints.Concat(l.Select((x,id) => new Point(Point.PType.LEFT, x,id)));
-            allPoints = allPoints.Concat(r.Select((x,id) => new Point(Point.PType.RIGHT, x,id)));
-
-            allPoints = allPoints.OrderBy(x => x.Location);
-
-            var intervalSet = new HashSet<int>();
-            var pointToIntervals = new Dictionary<int, List<int>>();
-
-            foreach(var point in allPoints)
+            foreach(int point in p)
             {
-                if(point.PointType == Point.PType.LEFT)
+                int found = -1;
+                for(int i = 0; i < segments.Count; i++)
                 {
-                    intervalSet.Add(point.ID);
+                    var seg = segments[i];
+                    if(seg.Item1 <= point && seg.Item2 >= point)
+                    {
+                        found = i;
+                        break;
+                    }
                 }
-                else if(point.PointType == Point.PType.RIGHT)
+
+                if(found == -1)
                 {
-                    intervalSet.Remove(point.ID);
+                    return "Impossible";
                 }
-                else
-                {
-                    pointToIntervals[point.ID] = new HashSet<int>(intervalSet);
-                }
+
+                segments.RemoveAt(found);
             }
 
-
-
-
-
- 
-
-        }
-
-        class Point
-        {
-            public enum PType { LEFT, RIGHT, POINT }
-            public PType PointType;
-            public int Location;
-            public int ID;
-
-            public Point(PType type, int location, int id)
-            {
-                PointType = type;
-                Location = location;
-                ID = id;
-            }
+            return "Possible";
         }
     }
 }
